e2abeb087ea1b185227be658e30bc8c3f1c74ad1
Sacramentix <benji.u1225u@gmail.com>
2023-05-05 21:11:43 +0200

    add some medal

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import cloudflare from "@astrojs/cloudflare";
+import image from '@astrojs/image';
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  integrations: [image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  })],
 });
